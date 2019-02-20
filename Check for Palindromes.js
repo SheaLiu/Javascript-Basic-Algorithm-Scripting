5549f60811f35a560fdad4e9c4461b93b02cc1f8
卷卷卷哒 <32973042+SheaLiu@users.noreply.github.com>
2019-02-20 11:27:55 +0800

    Update Check for Palindromes.js

--- Check for Palindromes.js
+++ Check for Palindromes.js
@@ -1,1 +1,13 @@
-   
+
+
+function palindrome(str) {
+  var myReg = /[^\w\s]|_/g;  //去除字符串中的标点符号以及下划线 
+  var strs;
+  strs = str.replace(myReg,'').replace(/\s/g,'').toLowerCase();
+  if(strs.split('').reverse().join('') == strs){  //reverse()是对数组中元素的翻转
+    return true;
+  }else{
+    return false;
+  }
+}
+palindrome("eye");
