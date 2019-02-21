be257732c0c527d5eab383fc6fc28a9f3b8f09f8
卷卷卷哒 <32973042+SheaLiu@users.noreply.github.com>
2019-02-21 15:32:34 +0800

    Create Chunky Monkey.js

---
+++ Chunky Monkey.js
@@ -0,0 +1,8 @@
+function chunk(arr, size) {
+  var newArr = [];
+  for(var i = 0;i < arr.length;i += size){
+    newArr.push(arr.slice(i,i+size));
+  }
+  return newArr;
+}
+chunk(["a", "b", "c", "d"], 2);
